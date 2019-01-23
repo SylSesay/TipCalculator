920d4b99c820883cb20f3d2cd04130a22212837f
Sylvia Sesay <sylviasesay@sylvias-macbook-pro.local>
2019-01-23 13:01:34 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Sylvia Sesay on 1/23/19.
+//  Copyright © 2019 Sylvia Sesay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
